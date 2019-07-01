bce662d3a826703eeca0d382c776ee7d8e7f8721
BassamAzizi <101010bassam@gmail.com>
2019-07-01 21:30:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ function init() {
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
  */
-google.setOnLoadCallback(init);
+//google.setOnLoadCallback(init);
 
 /* All of this functionality is heavily reliant upon the DOM, so we
  * place our code in the $() function to ensure it doesn't execute
